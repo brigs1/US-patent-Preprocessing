669729e32de6680f4429f9b19cab612e89f3a0c8
brigs1 <30627711+brigs1@users.noreply.github.com>
2018-06-02 16:17:33 +0900

    Add files via upload

---
+++ patent_separator_for(2001_2004)_number.py
@@ -0,0 +1,53 @@
+import re
+import os
+
+def writeToFile(patent_document, file_name):    
+    if not os.path.exists("output"):        
+        os.makedirs("output")
+        
+    file_name = "output/" + str(file_name)    
+    with open(file_name, "w") as patent_file:        
+        for line in patent_document:            
+            patent_file.write(line)
+            
+
+def getPatentNumber(patent_document):    
+    for line in patent_document:
+        if "<doc-number>" in line:            
+             
+            file_name_pattern = re.search("\d{11}", line)
+            file_name = file_name_pattern.group() 
+            return file_name
+
+    return 0
+
+patent_dir = "2001"
+file_list = os.listdir(patent_dir)
+
+for xml_source_file in file_list:
+    full_path = patent_dir + "/" + xml_source_file
+    if os.path.isfile(full_path):        
+
+        counter = 0
+           
+        with open(full_path, "r") as xml_bulk_file:               
+            contents = xml_bulk_file.readlines()                
+            start_position=0                
+            start_syntax = "<?xml version="              
+               
+            for line in contents:            
+                if start_syntax in str(line) and counter > 0:                                
+                    patent_document = contents[start_position:counter - 1]                
+                    file_name = getPatentNumber(patent_document)                
+                    writeToFile(patent_document, file_name)               
+                    start_position = counter                               
+                
+                counter = counter + 1          
+      
+
+            patent_document = contents[start_position:counter - 1]
+            file_name = getPatentNumber(patent_document)
+            writeToFile(patent_document, file_name)
+
+
+    
